87538e3d171e2aea3d0e4bec0f384100290f5591
Crowntraders-Academy <ity.dee48@gmail.com>
2025-11-28 11:25:51 +0300

    Create admin-config.example.js

---
+++ admin-config.example.js
@@ -0,0 +1,5 @@
+// Rename this file to admin-config.js and fill the values.
+// Do NOT commit admin-config.js if you want to hide your anon key.
+
+export const SUPABASE_URL = "https://eygjxxdgxzatchqgwvep.supabase.co";
+export const SUPABASE_ANON_KEY = "yeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5Z2p4eGRneHphdGNocWd3dmVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzODUyODIsImV4cCI6MjA3Nzk2MTI4Mn0.USOvTWBQ_kW-ty5XkTehYmPJKXeD2_hGGGM40G55tqg";
